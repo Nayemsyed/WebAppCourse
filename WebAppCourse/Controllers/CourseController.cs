8b171d34ec06bd123394b514f56ddb40e4cd5305
Nayemsyed <sdnayem04112000@gmail.com>
2024-01-31 15:44:44 +0530

    Add project files.

---
+++ WebAppCourse/Controllers/CourseController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCourse.Models;
+
+namespace WebAppCourse.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        static List<Course> coursesdata = new List<Course>()
+        {
+            new Course(){CId=1,CName="Ravi",CFee=12000,Technology="Java",CStartDate=new DateTime(day:01,month:09,year:2023),CEndDate=new DateTime(day:01,month:10,year:2023) },
+            new Course(){CId=2,CName="Raju",CFee=9000,Technology="C",CStartDate=new DateTime(day:01,month:06,year:2031),CEndDate=new DateTime(day:01,month:07,year:2023) },
+            new Course(){CId=3,CName="Sita",CFee=8000,Technology="C#",CStartDate=new DateTime(day:01,month:10,year:2023),CEndDate=new DateTime(day:01,month:11,year:2023) },
+            new Course(){CId=4,CName="Siri",CFee=6000,Technology="Python",CStartDate=new DateTime(day:01,month:09,year:2023),CEndDate=new DateTime(day:01,month:10,year:2023) },
+            new Course(){CId=5,CName="Sama",CFee=7500,Technology="Testing",CStartDate=new DateTime(day:01,month:07,year:2023),CEndDate=new DateTime(day:01,month:08,year:2023) }
+        };
+        public ActionResult Index()
+        {
+            return View(coursesdata);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Course());
+        }
+        [HttpPost]
+        public ActionResult Create(Course cou)
+        {
+            if (ModelState.IsValid)
+            {
+                coursesdata.Add(cou);
+                return RedirectToAction("Index");
+            }
+            return View(cou);
+        }
+    }
+}
